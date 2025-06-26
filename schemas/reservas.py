d1790d00049cc3fd41c582248ff522096d58af9d
Felipe Segovia <felipeak47.fs@gmail.com>
2025-06-26 08:57:36 -0300

    schemas reservas

--- schemas/reservas.py
+++ schemas/reservas.py
@@ -1,8 +1,11 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, PositiveInt, PastDate
 from typing import Optional, List
 
 
 class Reservas(BaseModel):
     id: Optional[int] = None
-
+    idUsuario=Optional[PositiveInt]
+    idPaquete= Optional[PositiveInt]
+    cantidad_personas= Optional[PositiveInt]
+    fecha_reserva= Optional[PastDate]
 #COMPLETAR FELI
