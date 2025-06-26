4e0f66ec1c87af318df9c6796b82f3129ec2927e
Felipe Segovia <felipeak47.fs@gmail.com>
2025-06-26 08:43:32 -0300

    tabla reservas

--- models/reservas.py
+++ models/reservas.py
@@ -1,5 +1,5 @@
 from config.database import Base
-from sqlalchemy import Column, Integer, String, Numeric, ForeignKey, Text
+from sqlalchemy import Column, Integer, String, Numeric, ForeignKey, Text, Date
 from sqlalchemy.orm import relationship
 
 
@@ -8,6 +8,10 @@ class Reservas(Base):
     __tablename__ = "reservas"
 
     id = Column(Integer, primary_key = True)
+    idUsuario=Column(Integer, ForeignKey('usuarios.id'), nullable=False)
+    idPaquete= Column(Integer, ForeignKey('paquetes.id'), nullable=False)
+    cantidad_personas= Column(Integer)
+    fecha_reserva= Column(Date)
 #COMPLETAR LO QUE FALTA FELI
 
 
