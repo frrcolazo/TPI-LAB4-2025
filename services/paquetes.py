60365114f927abecc80b7ae785309cd50d378579
Benjamín Peralta <benjaminp158@gmail.com>
2025-06-27 14:39:52 -0300

    Modificado los servicios para poder ser usados en los routers

--- services/paquetes.py
+++ services/paquetes.py
@@ -17,35 +17,31 @@ class PaquetesService:
         result = self.db.query(PaquetesModel).filter(PaquetesModel.id == id).first()
         return result
 
-    def get_paquetes_by_mail(self, correo):
+    def get_paquetes_by_destino(self, destino):
         result = (
-            self.db.query(PaquetesModel).filter(PaquetesModel.correo == correo).all()
-        )
-        return result
-
-    def get_paquetes_by_mail_first(self, correo):
-        result = (
-            self.db.query(PaquetesModel).filter(PaquetesModel.correo == correo).first()
+            self.db.query(PaquetesModel)
+            .filter(PaquetesModel.destino.nombre == destino)
+            .all()
         )
         return result
 
     def create_paquetes(self, Paquete: Paquetes):
-        new_producto = PaquetesModel(**Paquete.model_dump())
-        self.db.add(new_producto)
+        new_paquete = PaquetesModel(**Paquete.model_dump())
+        self.db.add(new_paquete)
         self.db.commit()
         return
 
-    def update_paquetes(self, id: int, data: Paquetes):
+    def update_paquetes(self, id: int, data: Paquetes) -> bool:
         query = (
             update(PaquetesModel)
             .where(PaquetesModel.id == id)
             .values(data.model_dump())
         )
-        self.db.execute(query)
+        updated = self.db.execute(query)
         self.db.commit()
-        return
+        return updated.rowcount > 0
 
-    def delete_paquetes(self, id: int):
-        self.db.query(PaquetesModel).filter(PaquetesModel.id == id).delete()
+    def delete_paquetes(self, id: int) -> bool:
+        deleted = self.db.query(PaquetesModel).filter(PaquetesModel.id == id).delete()
         self.db.commit()
-        return
+        return deleted > 0
